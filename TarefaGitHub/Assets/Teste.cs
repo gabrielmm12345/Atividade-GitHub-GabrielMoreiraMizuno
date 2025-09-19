cf1f593f60b3aac8329fe7982404303d829cfb29
Gabriel <gabriel.faculdade12345@gmail.com>
2025-09-19 15:50:04 -0300

    Commit inicial com projeto Unity

---
+++ TarefaGitHub/Assets/Teste.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Teste : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Olá GitHub");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
